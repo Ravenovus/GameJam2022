fbeb2b1d360554148ac3f65f0b68061d13336fd2
Ravenovus <voronov213@gmail.com>
2022-07-17 20:41:49 +0300

    Version 1.0

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] RectTransform credits;
+
+
+   
+    public void LoadFirstLevel()
     {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowCredits()
     {
-        
+        credits.gameObject.SetActive(true);
+    }
+
+    public void CloseCredits()
+    {
+        credits.gameObject.SetActive(false);
+    }
+
+    public void ShowJamPage()
+    {
+        Application.OpenURL("https://itch.io/jam/gmtk-jam-2022");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
