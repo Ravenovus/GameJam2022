9aef5c9ea9a935792228a04ce5eab95d41749109
Ravenovus <voronov213@gmail.com>
2022-07-17 10:32:20 +0300

    Audio Manager Implementation

--- Assets/Scripts/DiceMovementController.cs
+++ Assets/Scripts/DiceMovementController.cs
@@ -73,7 +73,7 @@ namespace GameJam.DiceManager
             if (!hasTouchedGround) { return; }
             if (!hasFullyStopped) { return; }
 
-            if(Input.GetMouseButtonDown(0) && hasTouchedGround)
+            if(Input.GetMouseButtonDown(0) && hasTouchedGround && diceRigidBody.velocity.normalized.Equals((0,0)))
             { 
                 ShowHand();
                 newLine = Instantiate(linePrefab);
@@ -114,6 +114,9 @@ namespace GameJam.DiceManager
         {
             if (collision.gameObject.tag == "Ground")
             {
+
+
+                AudioManager.audioManager.PlaySFX(UnityEngine.Random.Range(0, 14));
                 hasTouchedGround = true;                
             }
         }
